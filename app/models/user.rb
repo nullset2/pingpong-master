cbdfccac26ee3c70d3b973c60f43f62718dcc473
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-09-28 12:55:25 -0500

    all users are assigned a name exactly equal to their email name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
+  before_validation :compute_default_name
+
   scope :all_except, ->(u) { where.not(id: u) }
   devise :database_authenticatable, :registerable, :trackable, :validatable
   has_many :logged_games
 
   validates :name, presence: true
   validates :email, presence: true, format: { with: Devise.email_regexp }
+
+  def compute_default_name
+    self.name = self.email.split("@").first
+  end
 
 end
