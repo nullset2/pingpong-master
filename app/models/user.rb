f9c4243559280a7a94c0666fc538fbdbefa672e0
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-09-28 11:14:03 -0500

    adds basic user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   scope :all_except, ->(u) { where.not(id: u) }
   devise :database_authenticatable, :registerable, :trackable, :validatable
   has_many :logged_games
+
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: Devise.email_regexp }
+
 end
