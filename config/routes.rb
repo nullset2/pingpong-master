c3e7d64b0db30a2d84e529afcc3417528680da1e
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-09-28 11:59:32 -0500

    associates history route to controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :logged_games
   devise_for :users
   root to: "home#index"
-  get '/history', to: 'home#history'
+  get '/history', to: 'logged_games#index'
   get '/log',     to: 'logged_games#new'
 end
