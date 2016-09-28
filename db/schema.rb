5c2487dc9e079f242b4fcf7bedf2d51c0578d42f
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-09-28 11:08:52 -0500

    adds name to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928154133) do
+ActiveRecord::Schema.define(version: 20160928160035) do
 
   create_table "logged_games", force: :cascade do |t|
     t.datetime "date_played"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160928154133) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
