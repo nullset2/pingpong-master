e6105c23a051a1f03b78946a8df9ee95ecd742db
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-09-28 11:55:29 -0500

    improves validations for logged games

--- app/models/logged_game.rb
+++ app/models/logged_game.rb
@@ -2,12 +2,15 @@ class LoggedGame < ActiveRecord::Base
   belongs_to :user
   belongs_to :opponent, class_name: "User"
 
-  validates :user_score, numericality: { less_than_or_equal_to: 21 }
-  validates :opponent_score, numericality: { less_than_or_equal_to: 21 }
+  validates :user_score, presence: true, numericality: { less_than_or_equal_to: 21 }
+  validates :opponent_score, presence: true, numericality: { less_than_or_equal_to: 21 }
+  validates :opponent_id, presence: true
   validate :match_has_a_winning_score
 
   def match_has_a_winning_score
-    (self.user_score - self.opponent_score >= 2 ) && (self.user_score == 21 || self.opponent_score == 21) ? true : false
+    return true if ((self.user_score - self.opponent_score).abs >= 2 ) && (self.user_score == 21 || self.opponent_score == 21)
+    errors.add(:base, "Submitted scores are invalid, please check your input data")
+    false
   end
 
 end
