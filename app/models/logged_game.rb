a8ea7ba9af17d106e710b742de5ccf8066f04e0e
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-09-28 11:32:49 -0500

    adds business logic for a winning, true scores in logged game model

--- app/models/logged_game.rb
+++ app/models/logged_game.rb
@@ -1,4 +1,13 @@
 class LoggedGame < ActiveRecord::Base
   belongs_to :user
   belongs_to :opponent, class_name: "User"
+
+  validates :user_score, numericality: { less_than_or_equal_to: 21 }
+  validates :opponent_score, numericality: { less_than_or_equal_to: 21 }
+  validate :match_has_a_winning_score
+
+  def match_has_a_winning_score
+    (self.user_score - self.opponent_score >= 2 ) && (self.user_score == 21 || self.opponent_score == 21) ? true : false
+  end
+
 end
