51f8823ab315f8bb3180ec1fb4a8c56d7f28a05b
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-09-28 14:11:48 -0500

    sets root route to leaderboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :logged_games, except: [:edit, :destroy]
   devise_for :users
-  root to: "home#index"
+  root to: "users#index"
   get '/history', to: 'logged_games#index'
   get '/log',     to: 'logged_games#new'
 end
