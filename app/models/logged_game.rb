cc6df1ec50d606b12762fa89fcb99527a5a9fcd3
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-09-28 14:33:30 -0500

    intial version of ranking computation

--- app/models/logged_game.rb
+++ app/models/logged_game.rb
@@ -1,4 +1,5 @@
 class LoggedGame < ActiveRecord::Base
+  after_create :update_player_rankings
   belongs_to :user
   belongs_to :opponent, class_name: "User"
 
@@ -16,6 +17,27 @@ class LoggedGame < ActiveRecord::Base
   def status
     return "W" if self.user_score == 21
     "L"
+  end
+
+  def update_player_rankings
+    k_value = 32
+
+    player1 = User.find_by(id: self.user_id)
+    player2 = User.find_by(id: self.opponent_id)
+
+    player1_ranking = player1.ranking
+    player2_ranking = player2.ranking
+
+    self.status == "W" ? player1_result = 1 : player1_result = 0
+    player2_result = 1 - player1_result
+
+    player1_expectation = 1/(1+10**((player2_ranking - player1_ranking)/400.0))
+    player2_expectation = 1/(1+10**((player1_ranking - player2_ranking)/400.0))
+
+    self.user.ranking = (player1_ranking + k_value * (player1_result - player1_expectation)).round
+    self.opponent.ranking = (player2_ranking + k_value * (player2_result - player2_expectation)).round
+
+    byebug
   end
 
 end
