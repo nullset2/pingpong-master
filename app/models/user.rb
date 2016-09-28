766942d084872a588ffc8c23e71fac109efad743
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-09-28 11:10:29 -0500

    scope to fetch all users except one

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  scope :all_except, ->(u) { where.not(id: u) }
   devise :database_authenticatable, :registerable, :trackable, :validatable
   has_many :logged_games
 end
